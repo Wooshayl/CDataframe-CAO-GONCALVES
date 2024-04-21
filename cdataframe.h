bc9cf539682a2d037f785aefe102063c4b31d5ff
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2024-04-21 21:58:53 +0200

    Update cdataframe.h

--- cdataframe.h
+++ cdataframe.h
@@ -1,5 +1,8 @@
 
 #ifndef CDDATAFRAME_CDATAFRAME_H
 #define CDDATAFRAME_CDATAFRAME_H
+#include "column.h"
+Colonne** creer_cdataframe_vide();
+void remplissage_utilisateur_CDataframe(Colonne** CDataFrame);
 
 #endif //CDDATAFRAME_CDATAFRAME_H
