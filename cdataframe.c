7e97551bda341520dd55d6ee71dd3e848ac18e54
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2024-04-21 22:01:34 +0200

    Update cdataframe.c

--- cdataframe.c
+++ cdataframe.c
@@ -1,1 +1,21 @@
+
 #include "cdataframe.h"
+#include "column.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Colonne** creer_cdataframe_vide(){
+    Colonne** CDataFrame;
+    CDataFrame = (Colonne **)malloc(100 * sizeof(Colonne *));
+    for (int i = 0; i < 100; i++) {
+        CDataFrame[i] = (Colonne *)malloc(sizeof(Colonne));
+    }
+    return CDataFrame;
+}
+int taille_logique_cdataframe(Colonne** CDataFrame){
+    for (int i = 0; i < 100; ++i) {
+        if(CDataFrame[i] = NULL){
+            return i;
+        }
+    }
+}
