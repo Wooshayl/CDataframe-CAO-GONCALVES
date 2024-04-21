166eb1f392c9d4563852f6d46ef8596fe73e229c
valentino3112 <43887703+valentino3112@users.noreply.github.com>
2024-04-21 17:05:16 +0200

    Update column.h pour retourne_valeur_a_x

--- column.h
+++ column.h
@@ -12,6 +12,7 @@ typedef struct {
 
 Colonne *creer_colonne(char* titre);
 void print_col(Colonne* colonne);
+int retourne_valeur_a_x(Colonne* colonne, int x);
 int inserer_valeur(Colonne* colonne, int valeur);
 
 
