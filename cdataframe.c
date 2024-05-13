bc26f432b521707aa2f5025cbbad312cdf9d6545
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2024-05-13 13:47:32 +0200

    Update cdataframe.c

--- cdataframe.c
+++ cdataframe.c
@@ -3,6 +3,7 @@
 #include "column.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 Colonne** creer_cdataframe_vide(){
     Colonne** CDataFrame;
@@ -12,6 +13,7 @@ Colonne** creer_cdataframe_vide(){
     }
     return CDataFrame;
 }
+
 int taille_logique_cdataframe(Colonne** CDataFrame){
     for (int i = 0; i < 100; ++i) {
         if(CDataFrame[i] = NULL){
@@ -19,18 +21,24 @@ int taille_logique_cdataframe(Colonne** CDataFrame){
         }
     }
 }
-void remplissage_utilisateur_CDataframe(Colonne** CDataFrame){
-    printf("Combien de colonnes voulez-vous ?");
+Colonne** remplissage_utilisateur_CDataframe(int taille){
+    Colonne** CDataFrame = (Colonne**)malloc(taille*sizeof(Colonne*));
+    // printf("Combien de colonnes voulez-vous ?");
     int nbr_col, nbr_lig_max=0;
-    scanf("%d", &nbr_col);
-    for(int i =0; i < nbr_col; i++){
+    // scanf("%d", &nbr_col);
+    getchar();
+    for(int i =0; i < taille; i++) {
         printf("Quel titre voulez-vous pour la colonne %d : ", i);
         char titre[100];
         gets(titre);
+        //scanf("%s", titre);
         Colonne *colonne = creer_colonne(titre);
         CDataFrame[i] = colonne;
+       // printf("i: %d et titre : %s\n",i,CDataFrame[0]->titre);
     }
-    for (int i = 0; i < nbr_col; ++i) {
+    //printf("%s\n",CDataFrame[0]->titre);
+    //printf("%s\n",CDataFrame[1]->titre);
+    for (int i = 0; i < taille; ++i) {
         printf("Combien de données voulez-vous dans la colonne %d ? ", i);
         int nbr_lig;
         scanf("%d", &nbr_lig);
@@ -44,7 +52,7 @@ void remplissage_utilisateur_CDataframe(Colonne** CDataFrame){
             inserer_valeur(CDataFrame[i], valeur_ligne);
         }
     }
-    for (int i = 0; i < nbr_col; ++i) {
+    for (int i = 0; i < taille; ++i) {
         if(CDataFrame[i]->taille_logique < nbr_lig_max){
             for (int j = CDataFrame[i]->taille_logique; j < nbr_lig_max; ++j) {
                 inserer_valeur(CDataFrame[i],0);
@@ -52,6 +60,7 @@ void remplissage_utilisateur_CDataframe(Colonne** CDataFrame){
         }
 
     }
+    return CDataFrame;
 }
 
 void remplissage_dure(Colonne** CDataFrame){
@@ -68,8 +77,8 @@ void remplissage_dure(Colonne** CDataFrame){
     CDataFrame[0] = ma_colonne;
     CDataFrame[1] = ma_colonne2;
 }
-void affichage_cdatatframe(Colonne** CDataFrame){
-    for (int i = 0; i < 100; ++i) {
+void affichage_cdatatframe(Colonne** CDataFrame, int taille){
+    for (int i = 0; i < taille; ++i) {
         if(CDataFrame[i] != NULL){
             printf("%s", CDataFrame[i]->titre);
             for (int j = 0; j < CDataFrame[i]->taille_logique; ++j) {
@@ -109,8 +118,8 @@ void affichage_cdatatframe_colonne_utilisateur(Colonne** CDataFrame){
     }
 
 }
-void ajouter_ligne_CDataframe(Colonne** CDataFrame, int valeur){
-    for (int i = 0; i < 100; ++i) {
+void ajouter_ligne_CDataframe(Colonne** CDataFrame, int valeur, int taille){
+    for (int i = 0; i < taille; ++i) {
         inserer_valeur(CDataFrame[i], valeur);
     }
 }
@@ -129,8 +138,11 @@ void supprimer_colonne_CDataframe(Colonne** CDataFrame){
 void renommer_titre(Colonne** CDataFrame, int position){
     printf("Chosir un nouveau titre : ");
     char nouveau_titre[100];
+    fflush(stdout);
+    fflush(stdin);
     gets(nouveau_titre);
-    CDataFrame[position]->titre = nouveau_titre;
+    CDataFrame[position]->titre = (char*)realloc(CDataFrame[position]->titre, strlen(nouveau_titre)+1);
+    strcpy(CDataFrame[position]->titre, nouveau_titre);
 }
 int existence_valeur(Colonne** CDataFrame, int valeur){
     for (int i = 0; i < taille_logique_cdataframe(CDataFrame); ++i) {
@@ -144,9 +156,8 @@ int existence_valeur(Colonne** CDataFrame, int valeur){
 void changement_valeur(Colonne** CDataFrame, int colonne, int ligne, int valeur){
     CDataFrame[colonne]->donnees[ligne] = valeur;
 }
-
-void afficher_nom_colonne(Colonne** CDataFrame){
-    for (int i = 0; i < taille_logique_cdataframe(CDataFrame); ++i) {
+void afficher_nom_colonne(Colonne** CDataFrame, int taille){
+    for (int i = 0; i < taille; ++i) {
         printf("%s\n",CDataFrame[i]->titre);
     }
 }
@@ -168,7 +179,6 @@ int nombre_cellule_x(Colonne** CDataFrame, int x){
     }
     return cpt;
 }
-
 int nombre_cellule_superieur_x(Colonne** CDataFrame, int x){
     int cpt=0;
     for (int i = 0; i < 100; ++i) {
