a848c88af5fc926be013f81530d7ef22466d09f5
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2024-05-13 13:45:32 +0200

    Update cdataframe.h

--- cdataframe.h
+++ cdataframe.h
@@ -3,23 +3,22 @@
 #define CDDATAFRAME_CDATAFRAME_H
 #include "column.h"
 Colonne** creer_cdataframe_vide();
-void remplissage_utilisateur_CDataframe(Colonne** CDataFrame);
+Colonne** remplissage_utilisateur_CDataframe(int taille);
 void remplissage_dure(Colonne** CDataFrame);
-void affichage_cdatatframe(Colonne** CDataFrame);
+void affichage_cdatatframe(Colonne** CDataFrame, int taille);
 void affichage_cdatatframe_ligne_utilisateur(Colonne** CDataFrame);
 void affichage_cdatatframe_colonne_utilisateur(Colonne** CDataFrame);
-void ajouter_ligne_CDataframe(Colonne** CDataFrame, int valeur);
+void ajouter_ligne_CDataframe(Colonne** CDataFrame, int valeur, int taille);
 void supprimer_ligne_CDataframe(Colonne** CDataFrame);
 void ajouter_colonne_CDataframe(Colonne** CDataFrame);
 void supprimer_colonne_CDataframe(Colonne** CDataFrame);
 void renommer_titre(Colonne** CDataFrame, int position);
 int existence_valeur(Colonne** CDataFrame, int valeur);
 void changement_valeur(Colonne** CDataFrame, int colonne, int ligne, int valeur);
-void afficher_nom_colonne(Colonne** CDataFrame);
+void afficher_nom_colonne(Colonne** CDataFrame, int taille);
 void afficher_nombre_ligne(Colonne** CDataFrame);
 void afficher_nombre_colonne(Colonne** CDataFrame);
 int nombre_cellule_x(Colonne** CDataFrame, int x);
 int nombre_cellule_superieur_x(Colonne** CDataFrame, int x);
 int nombre_cellule_inferieur_x(Colonne** CDataFrame, int x);
-
 #endif //CDDATAFRAME_CDATAFRAME_H
