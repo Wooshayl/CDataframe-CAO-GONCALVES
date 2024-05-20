a5de9e802d3b7f7cce72f528f822f756a5ee2503
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2024-05-20 16:50:00 +0200

    Update cdataframe.h

--- cdataframe.h
+++ cdataframe.h
@@ -1,24 +1,28 @@
+/*======================================================================================================================
+                                CDataframe par Lilian CAO et Valentin GONCALVES
+Ce fichier contient les prototyeps des fonctions de 'cdataframe.c'.
+======================================================================================================================*/
 
 #ifndef CDDATAFRAME_CDATAFRAME_H
 #define CDDATAFRAME_CDATAFRAME_H
-#include "column.h"
+#include "colonne.h"
 Colonne** creer_cdataframe_vide();
 Colonne** remplissage_utilisateur_CDataframe(int taille);
 void remplissage_dure(Colonne** CDataFrame);
 void affichage_cdatatframe(Colonne** CDataFrame, int taille);
-void affichage_cdatatframe_ligne_utilisateur(Colonne** CDataFrame);
+void affichage_cdatatframe_ligne_utilisateur(Colonne** CDataFrame, int taille);
 void affichage_cdatatframe_colonne_utilisateur(Colonne** CDataFrame);
-void ajouter_ligne_CDataframe(Colonne** CDataFrame, int valeur, int taille);
-void supprimer_ligne_CDataframe(Colonne** CDataFrame);
-void ajouter_colonne_CDataframe(Colonne** CDataFrame);
+void ajouter_ligne_CDataframe(Colonne** CDataFrame, int taille);
+void supprimer_ligne_CDataframe(Colonne** CDataFrame, int taille);
+void ajouter_colonne_CDataframe(Colonne*** CDataFrame, int * taille);
 void supprimer_colonne_CDataframe(Colonne** CDataFrame);
 void renommer_titre(Colonne** CDataFrame, int position);
-int existence_valeur(Colonne** CDataFrame, int valeur);
+int existence_valeur(Colonne** CDataFrame,  void* valeur, int taille);
 void changement_valeur(Colonne** CDataFrame, int colonne, int ligne, int valeur);
 void afficher_nom_colonne(Colonne** CDataFrame, int taille);
 void afficher_nombre_ligne(Colonne** CDataFrame);
-void afficher_nombre_colonne(Colonne** CDataFrame);
-int nombre_cellule_x(Colonne** CDataFrame, int x);
-int nombre_cellule_superieur_x(Colonne** CDataFrame, int x);
-int nombre_cellule_inferieur_x(Colonne** CDataFrame, int x);
+void afficher_nombre_colonne(Colonne** CDataFrame,int taille);
+int nombre_cellule_x(Colonne** CDataFrame, void* x, int taille);
+int nombre_cellule_superieur_x(Colonne** CDataFrame, void* x, int taille);
+int nombre_cellule_inferieur_x(Colonne** CDataFrame, void* x,int taille);
 #endif //CDDATAFRAME_CDATAFRAME_H
